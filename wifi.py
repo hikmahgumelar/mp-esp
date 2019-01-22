cefb491707d17584b09b7e8177c4351f181f7efc
hikmahgumelar <hikmah.gumelar@coderscolony.com>
2019-01-22 16:48:04 +0700

    working remote from handphone

--- wifi.py
+++ wifi.py
@@ -1,18 +1,42 @@
 import machine
 import network
 import time
-lampu = machine.Pin(2, machine.Pin.OUT)
+from umqtt.simple import MQTTClient
+import ubinascii
+
+topic = b"esp"
+        
 ap = 'MG'
-password = 'everyoneispartofmg'
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-lampu.low()
+password = 'everyoneispartofmg'
+server = 'm16.cloudmqtt.com'
 wlan.connect(ap, password)
-if wlan.isconnected:
-   for i in range(10):
-      lampu.high()
-      time.sleep(0.5)
-      lampu.low()
-      time.sleep(0.5)
+state = 0
 
-lampu.high()
+def sub_cb(topic, msg):
+    global state
+    lampu = machine.Pin(2, machine.Pin.OUT)
+    print((topic, msg))
+    if msg == b"on":
+       lampu.value(0)
+       state = 1
+    elif msg == b"off":
+       lampu.value(1)
+       state = 0
+
+def main():
+   client_id = b"esp8266"
+   c = MQTTClient(client_id , server, port = 19998, user='zqrauhzm', password='WTWTREkXeBLb')
+   c.set_callback(sub_cb)
+   c.connect()
+   c.subscribe(topic)
+
+   try:
+      while 1:
+          c.wait_msg()
+   finally:
+       c.disconnect()
+
+if __name__ == "__main__":
+     main()
